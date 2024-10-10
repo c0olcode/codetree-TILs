6a648f35f1b699eea0d21de7435dd09fc98d5b8e
Song <yjyjsssong@gmail.com>
2024-10-10 23:00:54 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241010/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static final int MAX_N = 10;
+    public static int n;
+    public static int map[] = new int[MAX_N*2];
+    public static int min = Integer.MAX_VALUE;
+    public static int totalSum = 0;
+    public static ArrayList<Integer> select = new ArrayList<>();
+
+    public static int calc(){
+        int value = 0;
+        for (int i = 0; i < select.size(); i++){
+            value += map[select.get(i)];
+        }
+        int second = totalSum - value;
+
+        return Math.abs(value - second);
+    }
+
+
+    public static void dfs(int index, int cnt){
+        if (cnt == n){
+            // 리스트 속 숫자로 계산하기
+            min = Math.min(min, calc());
+            return;
+        }
+        
+        if (index == map.length) return;
+
+        // 해당 인덱스를 선택하지 않았을 때
+        dfs(index + 1, cnt);
+
+        // 선택했을 때
+        select.add(index);
+        dfs(index + 1, cnt + 1);
+        select.remove(select.size()-1);
+    }
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < 2*n; i++){
+            map[i] = Integer.parseInt(st.nextToken());
+            totalSum += map[i];
+        }
+
+        dfs(0,0);
+
+        System.out.print(min);
+    }
+}
