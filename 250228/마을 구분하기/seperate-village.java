47f6f98bd446af2ab6e502f24f82be00381a26bb
Song <yjyjsssong@gmail.com>
2025-02-28 22:50:09 +0900

    Title: 마을 구분하기, Time: 149ms, Memory: 12MB, Status: Passed - CodeTree

--- 250228/마을 구분하기/seperate-village.java
+++ 250228/마을 구분하기/seperate-village.java
@@ -1,38 +1,43 @@
-// BFS, DFS 두 방식을 고민했음... -> 두 가지 다 풀어봐야겠다. 
-
 import java.util.*;
 
 public class Main {
-    public static int n,people_cnt;
-    public static int dx[] = {0,1,0,-1};
-    public static int dy[] = {1,0,-1,0};
+    public static int n, people_cnt;
+    public static int dx[] = {0, 1, 0, -1};
+    public static int dy[] = {1, 0, -1, 0};
     public static int grid[][];
     public static boolean visit[][];
 
     public static ArrayList<Integer> cnt_list = new ArrayList<>();
 
-    public static boolean inRange(int x, int y){
-        return x >= 0 && x < n && y >= 0 && y <n;
+    public static boolean inRange(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
     }
 
-    public static boolean canGo(int x, int y){
-        return inRange(x,y) && !visit[x][y] && (grid[x][y]==1);
+    public static boolean canGo(int x, int y) {
+        return inRange(x, y) && !visit[x][y] && (grid[x][y] == 1);
     }
 
-    public static void dfs(int x, int y){
+    public static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        visit[x][y] = true;
+        people_cnt = 1;
 
-        // 상하좌우 탐색하기
-        for (int i =0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int curX = current[0], curY = current[1];
 
-            if (canGo(nx,ny)){
-                people_cnt++;
-                visit[nx][ny] = true;
-                dfs(nx,ny);
+            for (int i = 0; i < 4; i++) {
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
+
+                if (canGo(nx, ny)) {
+                    queue.add(new int[]{nx, ny});
+                    visit[nx][ny] = true;
+                    people_cnt++;
+                }
             }
         }
-
     }
 
     public static void main(String[] args) {
@@ -44,14 +49,11 @@ public class Main {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
-                
-        
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (canGo(i,j)){
-                    visit[i][j] = true;
-                    people_cnt = 1;
-                    dfs(i,j);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (canGo(i, j)) {
+                    bfs(i, j);
                     cnt_list.add(people_cnt);
                 }
             }
@@ -60,7 +62,7 @@ public class Main {
         Collections.sort(cnt_list);
 
         System.out.println(cnt_list.size());
-        for (Integer value : cnt_list){
+        for (Integer value : cnt_list) {
             System.out.println(value);
         }
     }
