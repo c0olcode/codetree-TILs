169d46f92b8872588ec2968680b57b81c528555a
Song <yjyjsssong@gmail.com>
2025-04-06 19:28:57 +0900

    Title: N개의 점 중 M개 고르기, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 250406/N개의 점 중 M개 고르기/choose-m-out-of-n-points.java
+++ 250406/N개의 점 중 M개 고르기/choose-m-out-of-n-points.java
@@ -21,7 +21,7 @@ public class Main {
         int max_dist = -1;
             // 이중 for문 돌려서 거리 하나씩 다 계산 하기
         for (int i = 0; i < m-1; i++){
-            for (int j = 0; j < m; j++){
+            for (int j = 1; j < m; j++){
                 Pair p1 = select_list.get(i);
                 Pair p2 = select_list.get(j);
                 int u_dist = (int)(Math.pow(p1.x-p2.x,2) + Math.pow(p1.y-p2.y,2));
