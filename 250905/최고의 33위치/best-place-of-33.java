80a944975ae894089568695686d8dbb9b490e135
Song <yjyjsssong@gmail.com>
2025-09-05 22:17:25 +0900

    Title: 최고의 33위치, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250905/최고의 33위치/best-place-of-33.java
+++ 250905/최고의 33위치/best-place-of-33.java
@@ -12,21 +12,21 @@ public class Main {
         // Please write your code here.
         int m_cnt = 0;
 
-        if (n==3) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if(grid[i][j] == 1) m_cnt++;
-                }
-            }
-            System.out.println(m_cnt);
-            return;
+        // if (n==3) {
+        //     for (int i = 0; i < n; i++) {
+        //         for (int j = 0; j < n; j++) {
+        //             if(grid[i][j] == 1) m_cnt++;
+        //         }
+        //     }
+        //     System.out.println(m_cnt);
+        //     return;
 
-        }
+        // }
 
 
         // 3*3 코드 -> 탐색 범위 그만큼 한정 짓기
-        for (int i = 0; i< n-3; i++){
-            for (int j = 0; j < n-3; j++){
+        for (int i = 0; i<= n-3; i++){
+            for (int j = 0; j <= n-3; j++){
                 int cnt = 0;
 
                 for (int k = i; k <= i+2; k++){
