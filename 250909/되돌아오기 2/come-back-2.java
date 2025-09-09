5c1948fcbaeff4567d808492880450b65affe627
Song <yjyjsssong@gmail.com>
2025-09-09 23:46:42 +0900

    Title: 되돌아오기 2, Time: 143ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250909/되돌아오기 2/come-back-2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String commands = sc.next();
+        // Please Write your code.
+
+        int x = 0;
+        int y = 0;
+        int n = commands.length();
+        int dx[] = {0,1,0,-1};
+        int dy[] = {1,0,-1,0};
+        int result = -1;
+        int t = 0;
+        int dir = 0;
+
+        for (int i = 0; i < n; i++){
+            if (commands.charAt(i) == 'F'){
+                x += dx[dir];
+                y += dy[dir];
+            }
+            else if (commands.charAt(i) == 'L'){
+                dir = (dir+3) % 4;
+            }
+            else{
+                dir = (dir+1) % 4;
+            }
+
+            t++;
+
+            if (x == 0 & y == 0){
+                result = t;
+                break;
+            }
+        }
+
+        System.out.println(t);
+    }
+}
