810608c80e9194d19336952923a02f2ea463f51c
Song <yjyjsssong@gmail.com>
2025-09-10 22:56:40 +0900

    Title: XOR 결과 최대 만들기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250910/XOR 결과 최대 만들기/max-of-xor.java
+++ 250910/XOR 결과 최대 만들기/max-of-xor.java
@@ -9,9 +9,10 @@ public class Main {
     public static void dfs(int index, int cnt){
         if (cnt == m){
             // xor 연산
-            int value = 0;
-            for (Integer i : list){
-                value = value ^ i;
+            int value = list.get(0);
+
+            for (int i =1; i < m; i++){
+                value = value ^ list.get(i);
             }
 
             max = Math.max(value,max);
