054ea96eb230a07953b2038ebc0a8c54b664dbbf
Song <yjyjsssong@gmail.com>
2025-11-02 20:16:37 +0900

    Title: N개 중에 M개 뽑기, Time: 112ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251102/N개 중에 M개 뽑기/n-choose-m.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n,m;
+    public static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void dfs(int index, int cnt){
+        if (cnt == m){
+            for (int i = 0; i < m; i++){
+                System.out.print(list.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+        if (index > n) return;
+        // 뽑
+        list.add(index);
+        dfs(index+1, cnt+1);
+        list.remove(list.size()-1);
+        // 안뽑
+        dfs(index+1, cnt);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        dfs(1,0);
+        // Please write your code here.
+    }
+}
